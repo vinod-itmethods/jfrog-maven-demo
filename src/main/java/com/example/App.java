4fd038ddd252ee72f2148dbf3a7e9d7853c533db
Vinod K. Avinane <vinod.avinane@itmethods.com>
2025-03-27 12:51:01 -0400

    test new methods

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+        String input = " Hello JFrog ";
+        System.out.println("Original: '" + input + "'");
+        System.out.println("Trimmed: '" + StringUtils.trim(input) + "'");
     }
 }
